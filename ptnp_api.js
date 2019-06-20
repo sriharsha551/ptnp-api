71c475b7963578d1c2cbe8e2aa2e20161a88b407
sriharsha551 <sanampudisriharsha@gmail.com>
2019-06-20 16:54:29 +0530

    filter

--- ptnp_api.js
+++ ptnp_api.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const Promise = require("promise");
 const app = express();
 const cors = require("cors");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const bodyParser = require("body-parser");
+const { getJsDateFromExcel } = require("excel-date-to-js");
 let mysql = require("mysql");
 let xlsx = require("node-xlsx");
 const port = 5000;
@@ -23,7 +23,8 @@ con.connect(function(err) {
 app.use(cors());
 app.use(bodyParser.json());
 
-app.put("/students/add", upload.array("file", 12), (req, res) => {
+app.post("/students/add", upload.array("file", 12), (req, res) => {
+  let returnData = {};
   let files = req.files;
   filenames = [];
   for (let i = 0; i < Object.keys(files).length; i++) {
@@ -32,16 +33,35 @@ app.put("/students/add", upload.array("file", 12), (req, res) => {
   filenames.forEach(path => {
     const obj = xlsx.parse(path);
     let records = obj[0].data;
+    let index = records[0].indexOf("DOB");
+    for (let i = 1; i < records.length; i++) {
+      records[i][index] = getJsDateFromExcel(records[i][index]);
+    }
     let values = [records[1], records[2], records[3]];
+    console;
     let sql = "insert into student_details (" + records[0] + ") values ?";
     con.query(sql, [values], function(err) {
       if (err) throw err;
     });
   });
-  res.send("Data Imported successfully");
+  returnData.status = "Data Imported Successfully!";
+  res.send(returnData);
+});
+
+app.get("/student/details", upload.none(), (req, res) => {
+  let returnData = {};
+  let sql = "select * from student_details";
+  let data = [];
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    data.push(JSON.parse(JSON.stringify(result)));
+    returnData.result = data;
+    res.send(returnData);
+  });
 });
 
 app.post("/drives/add", upload.none(), (req, res) => {
+  let returnData = {};
   let data = req.body.data;
   company = data["company"];
   round_id = data["round_id"];
@@ -83,39 +103,12 @@ app.post("/drives/add", upload.none(), (req, res) => {
       values[0].pop();
     }
   );
-  res.send("sucessfully added!");
-});
-
-app.put("/round/add", function(req, res) {
-  let data = req.body;
-  let sql = "insert into rounds (round_name) values ('" + data.data + "')";
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    //console.log(result);
-  });
-  res.send("Rounds added successfully");
-});
-
-app.get("/rounds", function(req, res) {
-  con.query("select * from rounds", (err, result) => {
-    if (err) throw err;
-    res.send(JSON.parse(JSON.stringify(result)));
-  });
-});
-
-app.post("/rounds/delete", (req, res) => {
-  data = req.body;
-  con.query("alter table rounds auto_increment = " + 1 + " ");
-  con.query(
-    "delete from rounds where id = (" + data.id + ") ",
-    (err, result) => {
-      if (err) throw err;
-    }
-  );
-  res.send("Deleted successfully!");
+  returnData.status = "Successfully added!";
+  res.send(returnData);
 });
 
 app.get("/drives/upcoming", (req, res) => {
+  let returnData = {};
   let date = new Date().toLocaleDateString("en-GB").split("/");
   date = [date[2], date[0], date[1]].join("-");
   let noOfDrives;
@@ -126,92 +119,283 @@ app.get("/drives/upcoming", (req, res) => {
   con.query(sql, (err, result) => {
     if (err) throw err;
     driveData = JSON.parse(JSON.stringify(result));
-    noOfDrives = driveData.length;
-    for (let i = 0; i < driveData.length; i++) {
-      let drive_id = driveData[i].drive_id;
-      let noOfRounds = driveData[i].no_of_rounds;
-      let sql =
-        "select round_id from drive_rounds where drive_id =" + drive_id + "";
-      con.query(sql, (error, resData) => {
-        let round_ids = JSON.parse(JSON.stringify(resData));
-        let round_list = [];
-        for (let j = 0; j < round_ids.length; j++) {
-          let sql =
-            "select id,round_name from rounds where id=" +
-            round_ids[j].round_id +
-            "";
-          con.query(sql, (err, result) => {
-            round_list.push(result[0]);
-            driveData[i]["rounds"] = round_list;
-            if (noOfRounds - 1 === j) {
-              updateDrive(driveData, i);
-            }
-          });
-        }
-      });
+    if (driveData.length === 0) {
+      returnData.result = driveData;
+      res.send(returnData);
+    } else {
+      noOfDrives = driveData.length;
+      for (let i = 0; i < driveData.length; i++) {
+        let drive_id = driveData[i].drive_id;
+        let noOfRounds = driveData[i].no_of_rounds;
+        let sql =
+          "select round_id from drive_rounds where drive_id =" +
+          drive_id +
+          " and delete_status='0'";
+        con.query(sql, (error, resData) => {
+          let round_ids = JSON.parse(JSON.stringify(resData));
+          let round_list = [];
+          for (let j = 0; j < round_ids.length; j++) {
+            let sql =
+              "select id,round_name from rounds where id=" +
+              round_ids[j].round_id +
+              "";
+            con.query(sql, (err, result) => {
+              round_list.push(result[0]);
+              +9;
+              driveData[i]["rounds"] = round_list;
+              if (noOfRounds - 1 === j) {
+                updateDrive(driveData, i);
+              }
+            });
+          }
+        });
+      }
     }
   });
   function updateDrive(driveData, i) {
     if (i === noOfDrives - 1) {
-      res.send(driveData);
+      returnData.result = driveData;
+      res.send(returnData);
     }
   }
 });
 
 app.post("/drives/delete", (req, res) => {
+  let returnData = {};
   let data = req.body.data;
-  let sql ="update drive_details set delete_status='1' where drive_id=" +data.drive_id +"";
+  let sql =
+    "update drive_details set delete_status='1' where drive_id=" +
+    data.drive_id +
+    "";
   con.query(sql, (err, driveResult) => {
     if (err) throw err;
   });
-  res.send('Deleted Successfully!');
+  returnData.status = "Deleted Successfully";
+  res.send(returnData);
+});
+
+app.post("/drives/rounds/delete", (req, res) => {
+  let returnData = {};
+  let data = req.body.data;
+  let noOfRounds = data.noOfRounds - 1;
+  if (noOfRounds >= 1) {
+    con.query(
+      "update drive_rounds set delete_status='1' where drive_id=" +
+        data.drive_id +
+        " and round_id=" +
+        data.round_id +
+        ""
+    );
+    con.query(
+      "update drive_details set no_of_rounds=" +
+        noOfRounds +
+        " where drive_id=" +
+        data.drive_id +
+        ""
+    );
+    returnData.status = "Successfully deleted!";
+    res.send(returnData);
+  } else {
+    returnData.status = "Atleast one round must be present!";
+    res.send(returnData);
+  }
 });
 
 app.post("/drives/modify", (req, res) => {
+  let returnData = {};
   let data = req.body.data;
-  let date = data.date.split('/');
-  date =[date[2], date[1],date[0]].join("-");
-  let dateQuery ="update drive_details set date_of_drive=DATE_FORMAT('" +date +"', '%Y-%m-%d') where drive_id="+data.drive_id+"";
-  con.query(dateQuery,(err,result)=>{
+  let date = data.date.split("/");
+  date = [date[2], date[1], date[0]].join("-");
+  let dateQuery =
+    "update drive_details set date_of_drive=DATE_FORMAT('" +
+    date +
+    "', '%Y-%m-%d') where drive_id=" +
+    data.drive_id +
+    "";
+  con.query(dateQuery, (err, result) => {
     if (err) throw err;
   });
-  con.query("select id from drive_rounds where drive_id="+data.drive_id+"",(err,ids)=>{
-    ids=(JSON.parse(JSON.stringify(ids)));
-    let noOfRounds = data.rounds.length
-    if(noOfRounds > ids.length){
-      values=[[data.drive_id,data.rounds[noOfRounds-1]]]
-      con.query("insert into drive_rounds (drive_id,round_id) values ?",[values]);
-      con.query("update drive_details set no_of_rounds="+noOfRounds+" where drive_id="+data.drive_id+"");
+  con.query(
+    "select id from drive_rounds where drive_id=" + data.drive_id + "",
+    (err, ids) => {
+      ids = JSON.parse(JSON.stringify(ids));
+      let noOfRounds = data.rounds.length;
+      if (noOfRounds > ids.length) {
+        values = [[data.drive_id, data.rounds[noOfRounds - 1]]];
+        con.query("insert into drive_rounds (drive_id,round_id) values ?", [
+          values
+        ]);
+        con.query(
+          "update drive_details set no_of_rounds=" +
+            noOfRounds +
+            " where drive_id=" +
+            data.drive_id +
+            ""
+        );
+      } else {
+        let id_list = [];
+        ids.forEach(element => {
+          id_list.push(element.id);
+        });
+        con.query(
+          "update drive_details set no_of_rounds=" +
+            ids.length +
+            " where drive_id=" +
+            data.drive_id +
+            ""
+        );
+        for (let i = 0; i < id_list.length; i++) {
+          let roundQuery =
+            "update drive_rounds set round_id =" +
+            data.rounds[i] +
+            " where id=" +
+            id_list[i] +
+            "";
+          con.query(roundQuery, (err, resultRound) => {
+            if (err) throw err;
+          });
+        }
+      }
     }
-    else{
-    let id_list = [];
-    ids.forEach((element)=>{id_list.push(element.id)});
-    con.query("update drive_details set no_of_rounds="+ids.length+" where drive_id="+data.drive_id+"");
-    for(let i=0;i<id_list.length;i++){
-        let roundQuery = "update drive_rounds set round_id ="+data.rounds[i]+" where id="+id_list[i]+"";
-        con.query(roundQuery,(err,resultRound)=>{
-          if (err) throw err;
+  );
+  returnData.status = "Successfully modified!";
+  res.send(returnData);
+});
+
+app.post("/drives/olddrive", (req, res) => {
+  let returnData = {};
+  let data = req.body.data;
+  let noOfDrives;
+  let sql =
+    "select * from drive_details where YEAR(date_of_drive)=" + data.year + "";
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    olddriveData = JSON.parse(JSON.stringify(result));
+    if (olddriveData.length === 0) {
+      returnData.result = driveData;
+      res.send(returnData);
+    } else {
+      noOfDrives = olddriveData.length;
+      for (let i = 0; i < olddriveData.length; i++) {
+        let drive_id = olddriveData[i].drive_id;
+        let noOfRounds = olddriveData[i].no_of_rounds;
+        let sql =
+          "select round_id from drive_rounds where drive_id =" +
+          drive_id +
+          " and delete_status='0'";
+        con.query(sql, (error, resData) => {
+          let round_ids = JSON.parse(JSON.stringify(resData));
+          let round_list = [];
+          for (let j = 0; j < round_ids.length; j++) {
+            let sql =
+              "select id,round_name from rounds where id=" +
+              round_ids[j].round_id +
+              "";
+            con.query(sql, (err, result) => {
+              round_list.push(result[0]);
+              +9;
+              olddriveData[i]["rounds"] = round_list;
+              if (noOfRounds - 1 === j) {
+                updateDrive(olddriveData, i);
+              }
+            });
+          }
         });
       }
     }
   });
-  res.send('successfully modified!');
+  function updateDrive(olddriveData, i) {
+    if (i === noOfDrives - 1) {
+      returnData.result = olddriveData;
+      res.send(returnData);
+    }
+  }
 });
 
-app.get("/passing/year",(req,res)=>{
-  let sql = "select * from passing_out_year";
-  con.query(sql,(err,result)=>{
-    res.send(JSON.parse(JSON.stringify(result)));
+app.post("/round/add", function(req, res) {
+  let returnData = {};
+  let data = req.body;
+  let sql = "insert into rounds (round_name) values ('" + data.data + "')";
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+  });
+  returnData.status = "Rounds added Successfully";
+  res.send(returnData);
+});
+
+app.get("/rounds", function(req, res) {
+  let returnData = {};
+  con.query("select * from rounds", (err, result) => {
+    if (err) throw err;
+    returnData = { result: JSON.parse(JSON.stringify(result)) };
+    res.send(returnData);
   });
 });
 
-app.get("/student/details", upload.none(), (req, res) => {
-  let sql = "select * from student_details";
-  let data = [];
+app.post("/rounds/delete", (req, res) => {
+  let returnData = {};
+  data = req.body;
+  con.query(
+    "update rounds set delete_status ='1' where id = (" + data.id + ") ",
+    (err, result) => {
+      if (err) throw err;
+    }
+  );
+  returnData.status = "Deleted Successfully";
+  res.send(returnData);
+});
+
+app.get("/passing/year", (req, res) => {
+  let returnData = {};
+  let sql = "select * from passing_out_year";
   con.query(sql, (err, result) => {
-    if (err) throw err;
-    data.push(JSON.parse(JSON.stringify(result)));
-    res.send(data);
+    returnData.result = JSON.parse(JSON.stringify(result));
+    res.send(returnData);
+  });
+});
+
+app.post("/students/filter", (req, res) => {
+  let returnData = {};
+  let data = req.body.data;
+  let branch=[];
+  let btech_name, inter_name, ssc_name;
+  data.branch.forEach((element)=>{
+    branch.push(parseInt(element));
+  });
+  branch=`${unescape(branch)}`;
+
+  if(data.isSelected ==='yes'){
+    data.isSelected="'0','1'";
+  }
+  else{
+    data.isSelected="'0'";
+  }
+  if (data.gender === 'all'){
+    data.gender= "'M','F'";
+  }
+  if (data.class10_score_type === "percentage") {
+    ssc_name = "SSC_PERCENTAGE";
+  } else {
+    ssc_name = "SSC_GPA";
+  }
+  if (data.class12_score_type === "percentage") {
+    inter_name = "INTER_PERCENTAGE";
+  } else {
+    inter_name = "INTER_CGPA";
+  }
+  if (data.btech_score_type === "cgpa") {
+    btech_name = "BTECH_CGPA";
+  } else {
+    btech_name = "BTECH_PERCENTAGE";
+  }
+
+  let sql ="select * from student_details where "+btech_name+">="+data.btech_score+" and "+inter_name+">="+data.class12_score+" and "+
+  ""+ssc_name+">="+data.class10_score+" and BTECH_BACKLOGS<="+data.backlogs+" and EAMCET_RANK<"+data.eamcet_rank+" and GENDER in ("+ data.gender +") "
+   +"and YOP_BTECH="+data.year_of_passing+" and selection_status in ("+data.isSelected+") and BRANCH_CODE in ("+branch+") ";
+  con.query(sql, (err, result) => {
+    result=(JSON.parse(JSON.stringify(result)));
+    returnData.result={result};
+    res.send(returnData);
   });
 });
 
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 // const excel = require("read-excel-file/node");
 let mysql = require("mysql");
-let xlsx = require("node-xlsx");
-
-const obj = xlsx.parse("../Students_details.xlsx");
-let records = obj[0].data;
-
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = 5000;
 
 let con = mysql.createConnection({
   host: "localhost",
@@ -12,94 +12,36 @@ let con = mysql.createConnection({
   password: "sriharsha12345",
   database: "pragati_tnp"
 });
-function connect (callback){
-con.connect(function(err) {
-  if (err) throw err;
-    let sql = "select * from drive_rounds";
-  con.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    callback();
+
+app.use(cors());
+app.use(bodyParser.json());
+con.connect();
+
+app.get("/drives/upcoming", (req, res) => {
+  let date = new Date().toLocaleDateString("en-GB").split("/");
+  date = [date[2], date[0], date[1]].join("-");
+  let sql = "select d.*,dr.round_id ,r.round_name from drive_details d"+
+  " inner join drive_rounds dr on d.drive_id = dr.drive_id inner join rounds r on r.id = dr.round_id"+
+  " where d.date_of_drive>=DATE_FORMAT('" +date +"', '%Y-%m-%d') and d.delete_status='0' order by d.drive_id";
+  con.query(sql,(err,result)=>{
+    let driveData = (JSON.parse(JSON.stringify(result)));
+    let drive_list = [];
+    for(let i=0;i<driveData.length;i++){
+      console.log(Object.keys(driveData[i]));
+      // if(driveData[i].keys() in drive_list[i]){console.log('hi')}
+      drive_list.push(driveData[i]);
+    }
   });
 });
-}
-function test(){
-console.log("hi");}
 
-connect(test);
-
-app.get("/drives/upcoming", (req, res) => {
-  let date = new Date().toLocaleDateString("en-GB").split("/");
-  date = [date[2], date[0], date[1]].join("-");
-  let apiData;
-  let sql =
-    "select * from drive_details where date_of_drive>=DATE_FORMAT('" +
-    date +
-    "', '%Y-%m-%d')";
-    con.query(sql, (err,result) => {
-      if (err) throw err;
-      let drive_list=[];
-      apiData = JSON.parse(JSON.stringify(result));
-      apiData.forEach((drive)=>{
-        let drive_id = drive.drive_id;
-        let noOfRounds = drive.no_of_rounds;
-        let sql = "select round_id from drive_rounds where drive_id ="+drive_id+"";
-        con.query(sql,(error,resData)=>{
-          let round_ids = JSON.parse(JSON.stringify(resData));
-          let round_list =[];
-          for(let i=1;i<=round_ids.length;i++){
-            let sql = "select round_name from rounds where id="+round_ids[i].round_id+"";
-            con.query(sql,(err,result)=>{
-              round_list.push(result[0].round_name);
-              drive['rounds']=round_list;
-              if (noOfRounds == i){
-              update(drive);
-              }
-            });
-          }
-        });
-      });
-      function update(driveData){
-        drive_list.push(driveData);
-        console.log(driveData);
-      }
-    });
+app.post('/students/filter',(req,res)=>{
+  let sql = "select * from student_details where "+btech_name+">="+data.btech_score+" and "+inter_name+">="+data.class12_score+" and "+
+  ""+ssc_name+">="+data.class10_score+"and BTECH_BACKLOGS<="+data.backlogs+" and EAMCET_RANK<"+data.eamcet_rank+"";
+  con.query(sql,(err,result)=>{
+    console.log(result);
+    // returnData.result={result};
+    // res.send(returnData);
+  });
 });
 
-
-
-
-app.get("/drives/upcoming", (req, res) => {
-  let date = new Date().toLocaleDateString("en-GB").split("/");
-  date = [date[2], date[0], date[1]].join("-");
-  let apiData;
-  let sql =
-    "select * from drive_details where date_of_drive>=DATE_FORMAT('" +
-    date +
-    "', '%Y-%m-%d')";
-    con.query(sql, (err,result) => {
-      if (err) throw err;
-      let drive_list=[];
-      apiData = JSON.parse(JSON.stringify(result));
-      apiData.forEach((drive)=>{
-        let drive_id = drive.drive_id;
-        let noOfRounds = drive.no_of_rounds;
-        let sql = "select round_id from drive_rounds where drive_id ="+drive_id+"";
-        con.query(sql,(error,resData)=>{
-          let round_ids = JSON.parse(JSON.stringify(resData));
-          let round_list =[];
-          for(let i=0;i<round_ids.length;i++){
-            let sql = "select round_name from rounds where id="+round_ids[i].round_id+"";
-            con.query(sql,(err,result)=>{
-              round_list.push(result[0].round_name);
-              drive['rounds']=round_list;
-              update(drive);
-            });
-          }
-        });
-      });
-      function update(driveData){
-        
-      }
-    });
-});
+app.listen(port);
